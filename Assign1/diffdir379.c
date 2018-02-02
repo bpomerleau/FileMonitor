07bcf9b0a96dd58bb871bcc1ccb14bbeffee6159
bpomerleau <bpomerle@ualberta.ca>
2018-02-01 17:38:16 -0700

    everything done except printing the contents of inotify

--- Assign1/diffdir379.c
+++ Assign1/diffdir379.c
@@ -10,61 +10,84 @@
 #include<string.h>
 #include<time.h>
 
-void timer_handler(int signal){
-	system("date");
-}
-
-void sigusr_handler(int signal){
-	printf("User interrupt!!\n");
-}
-
-void init(int period) {
-
-	//itimer sigaction init	
-	sigset_t timer_mask;
-	if (sigemptyset(&timer_mask) == -1) 
-		printf("Error on sigemptyset, errno: %s\n", strerror(errno));
-	if (sigaddset(&timer_mask, SIGUSR1) == -1) 
-		printf("Error on sigaddset(SIGUSR1), errno: %s\n", strerror(errno));
-	if (sigaddset(&timer_mask, SIGINT) == -1) 
-		printf("Error on sigaddset(SIGINT), errno: %s\n", strerror(errno));
-		
-	struct sigaction reg_report = { .sa_handler = timer_handler, .sa_mask = timer_mask }; 
-	
-	if (sigaction(SIGALRM, &reg_report, NULL) == -1) 
-		printf("Error on sigaction, errno: %s\n", strerror(errno));
-
-	//SIGUSR1 sigaction init	
-	sigset_t SIGUSR_mask;
-	if (sigemptyset(&SIGUSR_mask) == -1) 
-		printf("Error on sigemptyset, errno: %s\n", strerror(errno));
-	if (sigaddset(&SIGUSR_mask, SIGALRM) == -1) 
-		printf("Error on sigaddset(SIGALRM), errno: %s\n", strerror(errno));
-	if (sigaddset(&SIGUSR_mask, SIGINT) == -1) 
-		printf("Error on sigaddset(SIGINT), errno: %s\n", strerror(errno));	
-	
-	struct sigaction sigusr = { .sa_handler = sigusr_handler, .sa_mask = SIGUSR_mask }; 
-	
-	if (sigaction(SIGUSR1, &sigusr, NULL) == -1) 
-		printf("Error on sigaction, errno: %s\n", strerror(errno));
-	
-		
-	//itimter init
-	struct timeval interval = { .tv_sec = period };
-	const struct itimerval timer = { .it_interval = interval, .it_value = interval};
-
-	if (setitimer(ITIMER_REAL, &timer, NULL) == -1) 
-		printf("Error on setitimer. errno: %s\n", strerror(errno));
-	
-}
-
 int main(int argc, char *argv[]){
-
+	
+	sigset_t signal_mask;	
 	DIR *dirStream;
 	struct dirent *next;
-	int period;
+	struct inotify_event *inotify_buffer;
+	uint32_t mask;
 	char *path;
+	int period;
+	int printflag;	
+	int fd;
+	int wd;
 	char type;
+
+	void sig_handler(int signal){
+		system("date");
+		printflag = 1;
+	}
+
+	void init() {
+		//signal mask	
+		if (sigemptyset(&signal_mask) == -1) 
+			printf("Error on sigemptyset, errno: %s\n", strerror(errno));
+		if (sigaddset(&signal_mask, SIGUSR1) == -1) 
+			printf("Error on sigaddset(SIGUSR1), errno: %s\n", strerror(errno));
+		if (sigaddset(&signal_mask, SIGINT) == -1) 
+			printf("Error on sigaddset(SIGINT), errno: %s\n", strerror(errno));
+		if (sigaddset(&signal_mask, SIGALRM) == -1) 
+			printf("Error on sigaddset(SIGALRM), errno: %s\n", strerror(errno));		
+		
+		struct sigaction sig_struct = { .sa_handler = sig_handler, .sa_mask = signal_mask }; 
+		
+		//timer sigaction
+		if (sigaction(SIGALRM, &sig_struct, NULL) == -1) 
+			printf("Error on sigaction, errno: %s\n", strerror(errno));
+		//SIGUSR1 sigaction
+		if (sigaction(SIGUSR1, &sig_struct, NULL) == -1) 
+			printf("Error on sigaction, errno: %s\n", strerror(errno));
+	
+		
+		//itimter init
+		struct timeval interval = { .tv_sec = period };
+		const struct itimerval timer = { .it_interval = interval, .it_value = interval};
+
+		if (setitimer(ITIMER_REAL, &timer, NULL) == -1) 
+			printf("Error on setitimer. errno: %s\n", strerror(errno));
+	
+	}
+
+	int first_print() {
+		//first time print of everything in directory
+
+		dirStream = opendir(path);
+
+		if (dirStream < 0){
+			printf("%s not a valid directory\n", path);
+			return -1;
+		}
+
+		system("date"); //prints the date
+		next = readdir(dirStream);
+		while (next != NULL) {		
+			if (next->d_type == DT_DIR) 
+				type = 'd';
+			else if (next->d_type == DT_REG)
+				type = '+';
+			else
+				type = 'o';
+
+			if ((strcmp(next->d_name,".") != 0) && (strcmp(next->d_name,"..") != 0)) {
+				printf("%c %s\n", type, (*next).d_name);	
+			}
+			next = readdir(dirStream);
+		}
+	
+		closedir(dirStream);
+		return 0;
+	}
 	
 	if (argc == 3){
 		period = atoi(argv[1]);
@@ -77,51 +100,24 @@ int main(int argc, char *argv[]){
 		return 0;
 	}
 
-	init(period);	
+	init();	
+	if (first_print()==-1) return 0;	
 	
-	dirStream = opendir(path);
-	if (dirStream < 0){
-		printf("%s not a valid directory\n", path);
-		return 0;
-	}
+	//init inotify
+	inotify_buffer = malloc(sizeof(struct inotify_event) * 10 );
+	
+	mask = IN_CREATE | IN_DELETE | IN_MODIFY;
+	fd = inotify_init();
+	wd = inotify_add_watch(fd, path, mask);
 
-	//first time print of everything in directory
-	system("date");
-	next = readdir(dirStream);
-	while (next != NULL) {		
-		if (next->d_type == DT_DIR) 
-			type = 'd';
-		else if (next->d_type == DT_REG)
-			type = '+';
-		else
-			type = 'o';
-
-		if ((strcmp(next->d_name,".") != 0) && (strcmp(next->d_name,"..") != 0)) {
-			printf("%c %s\n", type, (*next).d_name);	
-		}
-		next = readdir(dirStream);
-	}
-	
-	closedir(dirStream);
-
-	struct inotify_event *inotify_buffer = malloc(sizeof(struct inotify_event) * 10 );
-	
-	uint32_t mask = IN_CREATE | IN_DELETE | IN_MODIFY;
-	int fd = inotify_init();
-	int wd = inotify_add_watch(fd, path, mask);
-	
-	//TODO -- instead of this while loop, we need to set up sigaction for 
-	//		  handling signals, and get inotify to send a signal when a
-	//		  change has occurred. As it is, read blocks, and we have to wait
-	//		  for a change for the process to continue
 	while (1) {	
-		printf("loop\n"); // for some reason if this line gets deleted something breaks
-		sleep(period);
+	sigprocmask(SIG_BLOCK, &signal_mask, NULL);
+	if (printflag == 1) {
+		printflag = 0;
 		
-		//TODO configure read to not block
-		//read(fd, inotify_buffer, sizeof(struct inotify_event)*10); //FIONREAD ioctl() for # available bytes
-		//printf("%i --- %s", (*inotify_buffer).wd, (*inotify_buffer).name);			
 	}
+	sigprocmask(SIG_UNBLOCK, &signal_mask, NULL);
+ }
 
 	free(inotify_buffer);
 	return 0;
